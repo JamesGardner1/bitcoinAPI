cd55f8d55ee45b0a28ac7a7c8c299b3fdae65fa1
JamesGardner1 <jamesacgardner@yahoo.com>
2019-11-11 21:03:42 -0600

    fixed test

--- bitcoin.py
+++ bitcoin.py
@@ -3,33 +3,35 @@ import requests
 
 def main():
     bitcoin = get_bitcoin_amount()
-    converted = convert_bitcoin_to_dollars()
+    converted = convert_bitcoin_to_dollars(bitcoin)
     display_results(bitcoin, converted)
 
 def get_bitcoin_amount():
     ''' Get Bitcoin Value '''
     return int(input('How much bitcoin do you possess?'))
 
-def request_rates():
-    data = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json').json()
-    return data
-
-def bitcoin_dollar_amount():
-    response = request_rates()
-    rate = extract_rate()
-    return rate
 
 def convert_bitcoin_to_dollars(bitcoin):
-    rate = extract_rate()
-    converted = convert(bitcoin, rate)
+    exchange_rate = get_exchange_rate()
+    converted = convert(bitcoin, exchange_rate)
     return converted
 
-def extract_rate():
-    return data['bpi']['USD']['rate_float']
+def get_exchange_rate():
+    response = request_rates()
+    rate = extract_rate(response)
+    return rate
 
-def convert(bitcoin, rate):
+def request_rates():
+    url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+    return requests.get(url).json()
+
+def extract_rate(rates):
+    print(rates)
+    return rates['bpi']['USD']['rate_float']
+
+def convert(bitcoin, exchange_rate):
     ''' Convert bitcoin to USD '''
-    return bitcoin * rate
+    return bitcoin * exchange_rate
 
 def display_results(bitcoin, converted):
     ''' Format and Display results. '''
--- testbitcoin.py
+++ testbitcoin.py
@@ -7,4 +7,10 @@ import bitcoin
 
 class TestExchangeRates(TestCase):
 
-    @patch('exchange')
+    @patch('bitcoin.request_rates')
+    def test_bitcoin_to_dollars(self, mock_rates)
+    mock_rate = 8753.8467
+    example_api_response = {'bpi': 'USD', 'rate_float': {'USD': mock_rate}}
+    mock_rate.side_effect = [ example_api_response ]
+    converted = exchange_rate.convert_bitcoin_to_dollars(4)
+    self.assertEqual(35015.39, converted)
